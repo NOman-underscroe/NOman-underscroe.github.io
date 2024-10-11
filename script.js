7643d28491cbc108b81ad8352facdb5aea389441
NOman-underscroe <shubh.hps@gmail.com>
2024-10-11 15:10:58 +0530

    Update script.js
    :)

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // Script to handle the file download
 document.getElementById("downloadBtn").addEventListener("click", function() {
     const link = document.createElement('a');
-    link.href = '/mnt/data/mickhand.ttf';  // Update with the correct path
+    link.href = 'E:/Mickey/mickhand.ttf';  // Update with the correct path
     link.download = 'mickhand.ttf';
     link.click();
 });
-
